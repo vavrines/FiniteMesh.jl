4d78ea7cf472f57a4d4c2f657c3e29657e7ca7eb
Tianbai Xiao <tianbaixiao@gmail.com>
2021-04-12 08:58:00 +0200

    Testing PyCall

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
 using Test
 import MeshArt
 
-MeshArt.read_mesh("t1.msh")
+#MeshArt.read_mesh("t1.msh")
+
+using PyCall
+meshio = pyimport("meshio")
+m0 = meshio.read("t1.msh")
+cells = m0.cells
+points = m0.points
